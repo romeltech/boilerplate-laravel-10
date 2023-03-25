5f53d1bfbf952f36116f6786fe7af1b80583e9a6
Mel <indemnedev@gmail.com>
2023-03-25 18:03:48 +0400

    Updated login

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/sanctumlogin', [UserApiController::class, 'login']);
+Route::get('/checkuser', [UserApiController::class, 'checkSanctumUser']);
