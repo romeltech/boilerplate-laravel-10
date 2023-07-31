33ef9d9ce7627e7febc64fb0a3c3d892aec1aa54
Romel Indemne <romel.i@gagroup.net>
2023-07-31 10:07:43 +0400

    updated packages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
 <body>
     <div id="app">
         <v-app style="background-color: #fafafa">
-            <main-component :auth="{{ json_encode(auth()->user()) }}"></main-component>
+            <main-component :auth="{{ json_encode(auth()->user() ? auth()->user() : null ) }}"></main-component>
             {{-- @auth @endauth --}}
         </v-app>
     </div>
