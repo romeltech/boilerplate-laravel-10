a4437870499c170bcde8b3974cf41f05af5eadc4
Romel Indemne <romel.i@gagroup.net>
2023-08-23 09:18:28 +0400

    updated manual import

---
+++ resources/js/vuetify/components.js
@@ -0,0 +1,161 @@
+import { VTooltip } from "vuetify/lib/components/index.mjs";
+import { VAlert } from "vuetify/lib/components/index.mjs";
+import { VAutocomplete } from "vuetify/lib/components/index.mjs";
+import { VCheckbox } from "vuetify/lib/components/index.mjs";
+import { VSelectionControl } from "vuetify/lib/components/index.mjs";
+import { VLabel } from "vuetify/lib/components/index.mjs";
+import { VSelectionControlGroup } from "vuetify/lib/components/index.mjs";
+import { VInput } from "vuetify/lib/components/index.mjs";
+import { VMessages } from "vuetify/lib/components/index.mjs";
+import { VChip } from "vuetify/lib/components/index.mjs";
+import { VChipGroup } from "vuetify/lib/components/index.mjs";
+import { VSelect } from "vuetify/lib/components/index.mjs";
+import { VTextField } from "vuetify/lib/components/index.mjs";
+import { VCounter } from "vuetify/lib/components/index.mjs";
+import { VField } from "vuetify/lib/components/index.mjs";
+import { VVirtualScroll } from "vuetify/lib/components/index.mjs";
+import { VBadge } from "vuetify/lib/components/index.mjs";
+import { VBanner } from "vuetify/lib/components/index.mjs";
+import { VBottomNavigation } from "vuetify/lib/components/index.mjs";
+import { VBreadcrumbs } from "vuetify/lib/components/index.mjs";
+import { VCarousel } from "vuetify/lib/components/index.mjs";
+import { VWindow } from "vuetify/lib/components/index.mjs";
+import { VCode } from "vuetify/lib/components/index.mjs";
+import { VColorPicker } from "vuetify/lib/components/index.mjs";
+import { VSlider } from "vuetify/lib/components/index.mjs";
+import { VSheet } from "vuetify/lib/components/index.mjs";
+import { VCombobox } from "vuetify/lib/components/index.mjs";
+import { VDialog } from "vuetify/lib/components/index.mjs";
+import { VExpansionPanel } from "vuetify/lib/components/index.mjs";
+import { VFileInput } from "vuetify/lib/components/index.mjs";
+import { VFooter } from "vuetify/lib/components/index.mjs";
+import { VItemGroup } from "vuetify/lib/components/index.mjs";
+import { VKbd } from "vuetify/lib/components/index.mjs";
+import { VLayout } from "vuetify/lib/components/index.mjs";
+import { VLayoutItem } from "vuetify/lib/components/index.mjs";
+import { VLocaleProvider } from "vuetify/lib/components/index.mjs";
+import { VPagination } from "vuetify/lib/components/index.mjs";
+import { VParallax } from "vuetify/lib/components/index.mjs";
+import { VRadioGroup } from "vuetify/lib/components/index.mjs";
+import { VSlideGroup } from "vuetify/lib/components/index.mjs";
+import { VSnackbar } from "vuetify/lib/components/index.mjs";
+import { VSwitch } from "vuetify/lib/components/index.mjs";
+import { VSystemBar } from "vuetify/lib/components/index.mjs";
+import { VTabs } from "vuetify/lib/components/index.mjs";
+import { VTab } from "vuetify/lib/components/index.mjs";
+import { VTable } from "vuetify/lib/components/index.mjs";
+import { VTextarea } from "vuetify/lib/components/index.mjs";
+import { VThemeProvider } from "vuetify/lib/components/index.mjs";
+import { VTimeline } from "vuetify/lib/components/index.mjs";
+import { VMain } from "vuetify/lib/components/index.mjs";
+import { VApp } from "vuetify/lib/components/index.mjs";
+import { VAppBar } from "vuetify/lib/components/index.mjs";
+import { VList } from "vuetify/lib/components/index.mjs";
+import { VListItem } from "vuetify/lib/components/index.mjs";
+import { VMenu } from "vuetify/lib/components/index.mjs";
+import { VOverlay } from "vuetify/lib/components/index.mjs";
+import { VNavigationDrawer } from "vuetify/lib/components/index.mjs";
+import { VDivider } from "vuetify/lib/components/index.mjs";
+import { VAvatar } from "vuetify/lib/components/index.mjs";
+import { VRating } from "vuetify/lib/components/index.mjs";
+import { VCard } from "vuetify/lib/components/index.mjs";
+import { VBtnGroup } from "vuetify/lib/components/index.mjs";
+import { VBtnToggle } from "vuetify/lib/components/index.mjs";
+import { VProgressCircular } from "vuetify/lib/components/index.mjs";
+import { VBtn } from "vuetify/lib/components/index.mjs";
+import { VProgressLinear } from "vuetify/lib/components/index.mjs";
+import { VIcon } from "vuetify/lib/components/index.mjs";
+import { VToolbar } from "vuetify/lib/components/index.mjs";
+import { VResponsive } from "vuetify/lib/components/index.mjs";
+import { VImg } from "vuetify/lib/components/index.mjs";
+// import { VColorPickerCanvas } from "vuetify/lib/components/index.mjs";
+// import { VColorPickerEdit } from "vuetify/lib/components/index.mjs";
+// import { VColorPickerPreview } from "vuetify/lib/components/index.mjs";
+// import { VSliderThumb } from "vuetify/lib/components/index.mjs";
+// import { VSliderTrack } from "vuetify/lib/components/index.mjs";
+// import { VColorPickerSwatches } from "vuetify/lib/components/index.mjs";
+// import { VRipple } from "vuetify/lib/components/index.mjs";
+// import { VGrid } from "vuetify/lib/components/index.mjs";
+const components = {
+    VCard,
+    VToolbar,
+    VAlert,
+    VAutocomplete,
+    VCheckbox,
+    VSelectionControl,
+    VLabel,
+    VSelectionControlGroup,
+    VInput,
+    VMessages,
+    VChip,
+    VChipGroup,
+    VSelect,
+    VTextField,
+    VCounter,
+    VField,
+    VVirtualScroll,
+    VBadge,
+    VBanner,
+    VBottomNavigation,
+    VBreadcrumbs,
+    VCarousel,
+    VWindow,
+    VCode,
+    VColorPicker,
+    VSlider,
+    VSheet,
+    VCombobox,
+    VDialog,
+    VExpansionPanel,
+    VFileInput,
+    VFooter,
+    VItemGroup,
+    VKbd,
+    VLayout,
+    VLayoutItem,
+    VLocaleProvider,
+    VPagination,
+    VParallax,
+    VRadioGroup,
+    VSlideGroup,
+    VSnackbar,
+    VSwitch,
+    VSystemBar,
+    VTabs,
+    VTab,
+    VTable,
+    VTextarea,
+    VThemeProvider,
+    VTimeline,
+    VTooltip,
+    VMain,
+    VApp,
+    VAppBar,
+    VList,
+    VListItem,
+    VMenu,
+    VOverlay,
+    VNavigationDrawer,
+    VDivider,
+    VAvatar,
+    VRating,
+    VCard,
+    VBtnGroup,
+    VBtnToggle,
+    VProgressCircular,
+    VBtn,
+    VProgressLinear,
+    VIcon,
+    VToolbar,
+    VResponsive,
+    VImg,
+    // VColorPickerCanvas,
+    // VColorPickerEdit,
+    // VColorPickerPreview,
+    // VSliderThumb,
+    // VSliderTrack,
+    // VColorPickerSwatches,
+    // VRipple,
+    // VGrid,
+};
+export default components;
--- resources/js/vuetify/vuetify.js
+++ resources/js/vuetify/vuetify.js
@@ -1,8 +1,11 @@
 // Vuetify
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+
+// components
+// import components from "./components"; // manual imports
+import * as components from "vuetify/components";
 
 // icon library - https://pictogrammers.com/library/mdi/
 import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
@@ -14,35 +17,19 @@ const vuetify = createVuetify({
         defaultTheme: "light",
         themes: {
             light: {
-                // dark: false,
                 colors: {
                     primary: "#000000",
                     secondary: "#C6A92D",
                     background: "#fafafa",
                     white: "#FFFFFF",
-                    // Vuetify Action Colors
-                    // secondary: '#f5f5f5',
-                    // accent: '#82B1FF',
-                    // error: '#FF5252',
-                    // info: '#2196F3',
-                    // success: "#4CAF50",
-                    // warning: '#FFC107',
                 },
             },
             dark: {
-                // dark: false,
                 colors: {
                     primary: "#FFFFFF",
                     secondary: "#C6A92D",
                     background: "#3c3c3c",
                     white: "#000000",
-                    // Vuetify Action Colors
-                    // secondary: '#f5f5f5',
-                    // accent: '#82B1FF',
-                    // error: '#FF5252',
-                    // info: '#2196F3',
-                    // success: "#4CAF50",
-                    // warning: '#FFC107',
                 },
             },
         },
